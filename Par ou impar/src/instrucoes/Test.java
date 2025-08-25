985db9b7a437d0486f4a533fb854453846a9055a
Nauan Silva <124005272+Nato1nn@users.noreply.github.com>
2025-08-25 18:39:47 -0300

    Add files via upload

---
+++ Par ou impar/src/instrucoes/Test.java
@@ -0,0 +1,28 @@
+package instrucoes;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int n1 = scanner.nextInt();
+
+        if (n1 > 0) {
+            System.out.println("O número é positivo.");
+        } else if (n1 < 0) {
+            System.out.println("O número é negativo.");
+        } else {
+            System.out.println("O número é zero.");
+        }
+
+        if (n1 % 2 == 0) {
+            System.out.println("O número é par.");
+        } else {
+            System.out.println("O número é impar.");
+        }
+
+        Scanner close;
+    }
+}
