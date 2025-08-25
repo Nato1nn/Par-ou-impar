8f46f5a05340ca12675a8350cce49762ffc7a32f
Nauan Silva <124005272+Nato1nn@users.noreply.github.com>
2025-08-25 18:53:32 -0300

    Update Test.java

--- Par ou impar/src/instrucoes/Test.java
+++ Par ou impar/src/instrucoes/Test.java
@@ -2,7 +2,7 @@ package instrucoes;
 
 import java.util.Scanner;
 
-public class Test {
+public class Codigo {
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
 
@@ -25,4 +25,5 @@ public class Test {
 
         Scanner close;
     }
+
 }
